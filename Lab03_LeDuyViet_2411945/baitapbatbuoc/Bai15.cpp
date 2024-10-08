80c149b8eacc49c301bb8df74b8a0f771bfb14b1
VietUniverse <88927084+VietUniverse@users.noreply.github.com>
2024-10-08 15:13:05 +0700

    lab3 done

---
+++ Lab03_LeDuyViet_2411945/baitapbatbuoc/Bai15.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double M, N, T;
+    int n;
+
+    // Nhập thông tin từ người dùng
+    cout << "Nhap gia xe: ";
+    cin >> M;
+    cout << "Nhap so tien tra truoc: ";
+    cin >> N;
+    cout << "Nhap lai xuat %: ";
+    cin >> T;
+    cout << "Nhap so tien moi thang tra: ";
+    cin >> n;
+
+    double P = (M - N) / n + (T / 100) * (M - N);
+    int monthsToPayOff = round((M - N) / P);
+    double lastMonthPayment = M - (monthsToPayOff - 1) * P;
+
+    cout << "So thang can tra het no: " << monthsToPayOff << endl;
+    cout << "So tien trong thang cuoi cung: " << lastMonthPayment << endl;
+
+    return 0;
+}
