eb3b90f456992d97bb0425b652e93c64b79ea734
VietUniverse <88927084+VietUniverse@users.noreply.github.com>
2024-10-08 15:42:58 +0700

    update bai15

--- Lab03_LeDuyViet_2411945/baitapbatbuoc/Bai15.cpp
+++ Lab03_LeDuyViet_2411945/baitapbatbuoc/Bai15.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
+#include <conio.h>
 
 using namespace std;
+void TraGop(long m, long n, long x, int t);
 
 int main() {
-    double M, N, T;
-    int n;
+    long m,n,x; int t;
     cout << "Nhap gia xe: ";
-    cin >> M;
+    cin >> m;
     cout << "Nhap so tien tra truoc: ";
-    cin >> N;
-    cout << "Nhap lai xuat %: ";
-    cin >> T;
-    cout << "Nhap so tien moi thang tra: ";
     cin >> n;
-
-    double P = (M - N) / n + (T / 100) * (M - N);
-    int monthsToPayOff = round((M - N) / P);
-    double lastMonthPayment = M - (monthsToPayOff - 1) * P;
-
-    cout << "So thang can tra het no: " << monthsToPayOff << endl;
-    cout << "So tien trong thang cuoi cung: " << lastMonthPayment << endl;
-
+    cout << "Nhap lai xuat %: ";
+    cin >> t;
+    cout << "Nhap so tien moi thang tra: ";
+    cin >> x;
+    TraGop(m,n,x,t);
+    _getch;
     return 0;
+}
+void TraGop(long m, long n, long x, int t)
+{
+    long debt = m - n; int count = 0; long lastPayment;
+    while (debt > 0)
+    {
+        if (debt - x > 0){
+            debt -= x;
+            debt += debt*t/100;
+            count ++;
+        }
+        else{
+            lastPayment = debt;
+            break;
+        }
+    }
+    cout << "\nSo thang can de tra het no la: " << count;
+    cout << "\nThang cuoi can phai tra so tien la: " << lastPayment;
 }
